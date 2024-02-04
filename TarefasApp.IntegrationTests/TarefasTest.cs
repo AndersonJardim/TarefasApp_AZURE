14014efb629acfc57f9ff598dfb35f93aaa989d9
Anderson Jardim <andersonjardim@gmail.com>
2024-02-04 09:33:14 -0300

    TarefasApp.IntegrationTests - Post / Put / Get / Delete
    PostTarefas_ReturnsSuccesStatusCode
    PutTarefas_ReturnsSuccesStatusCode
    DeleteTarefas_ReturnsSuccesStatusCode
    GetTarefas_ReturnsSuccesStatusCode

--- TarefasApp.IntegrationTests/TarefasTest.cs
+++ TarefasApp.IntegrationTests/TarefasTest.cs
@@ -1,11 +1,33 @@
 namespace TarefasApp.IntegrationTests
 {
-    public class UnitTest1
+    /// <summary>
+    /// Testes de integração do ENDPOINT da API de tarefas
+    /// </summary>
+    public class TarefasTest
     {
-        [Fact]
-        public void Test1()
+        [Fact(Skip = "Não implementado.")]
+        public async Task PostTarefas_ReturnsSuccesStatusCode()
         {
+            //await Task.Run();
+            await Task.CompletedTask;
+        }
 
+        [Fact(Skip = "Não implementado.")]
+        public async Task PutTarefas_ReturnsSuccesStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task DeleteTarefas_ReturnsSuccesStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task GetTarefas_ReturnsSuccesStatusCode()
+        {
+            await Task.CompletedTask;
         }
     }
 }
