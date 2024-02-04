0e12d2922abee9871d44ca455cf68442d6007f34
Anderson Jardim <andersonjardim@gmail.com>
2024-02-04 10:34:25 -0300

    Programs.cs
    Refatorando o Programs.cs
    TarefasApp.API.Extensions

--- TarefasApp.API/Program.cs
+++ TarefasApp.API/Program.cs
@@ -1,23 +1,14 @@
+using TarefasApp.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddSwaggerDoc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwaggerDoc();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
